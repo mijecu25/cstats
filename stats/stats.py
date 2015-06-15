45f1b2a3e7e7eab5becc3789ce7483ea5d91e56d
Miguel Velez <miguelvelez@mijecu25.com>
2015-06-14 22:06:40 -0400

    - Passing arguments to main

--- stats/stats.py
+++ stats/stats.py
@@ -1,9 +1,21 @@
+import sys as system
+
 __author__ = 'Miguel Velez'
 
+
 # Main method
-def main():
-    print 'Welcome'
+def main(args):
+    print
+    print 'stats provides statistic and information about your directory'
+
+    # Check if the user entered any arguments
+    if len(args) == 0:
+        pass
+    else:
+        # Loop through the arguments
+        for arg in args:
+            print arg
 
 # Start the program
 if __name__ == '__main__':
-    main()
+    main(system.argv[1:])
