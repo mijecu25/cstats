4a9780b3d854d0e09afa6246f005f6733500bb39
Miguel Velez <miguelvelez@mijecu25.com>
2015-06-18 16:21:30 -0400

    Using docopt

--- stats/stats.py
+++ stats/stats.py
@@ -1,21 +1,57 @@
+r"""STATS
+
+stats provides statistic and information about your directories
+
+Usage:
+    stats (ls | list) [<path>]
+    stats -h | --help
+
+Options:
+  -h --help        Show this screen.
+  --version     Show version.
+"""
+
+# Refer to http://docopt.org/ for documentation on how to structure the docopt
+
 import sys as system
+import os
+
+from docopt import docopt
 
 __author__ = 'Miguel Velez'
+__version__ = '0.1.1'
+
+
+def _list_files(path):
+    """
+    List the files and directories in the specified path
+    """
+    files = os.listdir(path)
+
+    for entry in files:
+        print entry
 
 
 # Main method
 def main(args):
-    print
-    print 'stats provides statistic and information about your directory'
+    """
+    Main Method of the stats program.
+    """
 
-    # Check if the user entered any arguments
-    if len(args) == 0:
-        pass
-    else:
-        # Loop through the arguments
-        for arg in args:
-            print arg
+    # Get the arguments from docopt
+    arguments = docopt(__doc__, version=('stats ' + __version__))
+
+    # If the user wants to list the files
+    if arguments['ls'] or arguments['list']:
+        # If the user did not specify a path
+        if len(args) == 0:
+            # Pass the current path
+            _list_files('.')
+        else:
+            # Else, pass the path provided by the user
+            _list_files(args[0])
+
 
 # Start the program
 if __name__ == '__main__':
-    main(system.argv[1:])
+    main(system.argv[2:])
