aca9a670b9fc4b6944325770f998d32ebe3be106
Miguel Velez <miguelvelez@mijecu25.com>
2015-07-27 16:22:44 -0400

    Added support for optional paths

--- stats/stats.py
+++ stats/stats.py
@@ -4,12 +4,13 @@ stats provides statistic and information about your directories
 
 Usage:
     stats (ls | list) [<path>]
+    stats (s | size) [<path>]
     stats -h | --help
-    stats -s | --size
+    stats -v | --version
 
 Options:
   -h --help     Show this screen.
-  --version     Show version.
+  -v --version     Show version.
 """
 
 # Refer to http://docopt.org/ for documentation on how to structure the docopt
@@ -21,16 +22,35 @@ import os
 from docopt import docopt
 
 __author__ = 'Miguel Velez'
-__version__ = '0.1.2.2'
+__version__ = '0.1.2.3'
 
 
-def _get_entry_info(entry):
-    return os.stat(entry)
+def _get_entry_info(entry, path):
+    """
+    Get information and attributes from the entry in the
+    specified path.
+    """
+
+    # If the path ends with a '/'
+    if path[-1:] == '/':
+        # Remove the '/'
+        path = path[:-1]
+
+    # Try
+    try:
+        # Get information and attributes from the entry in the path
+        info = os.stat(path + '/' + entry)
+    except WindowsError:
+        # The might be some windows error, so set the result to None
+        info = None
+
+    # Return the information
+    return info
 
 
 def list_files(path):
     """
-    List the files and directories in the specified path
+    List the files and directories in the specified path.
     """
 
     # Get the files and directories in the path
@@ -39,36 +59,52 @@ def list_files(path):
     # Sort the files by alphabetical order
     sorted(files)
 
-    # Print the files
+    # Loop through each file
     for entry in files:
         # Get some file information
-        # try:
-            file_info = _get_entry_info(entry)
+        file_info = _get_entry_info(entry, path)
 
-            print entry.ljust(20),\
-                str(file_info.st_size).ljust(10),\
-                time.ctime(file_info.st_ctime).ljust(30),\
-                time.ctime(file_info.st_mtime).ljust(30)
+        # If there is no file info
+        if file_info is None:
+            # Just jump to the next iteration of the loop
+            continue
 
-        # except WindowsError:
-        #     print 'Could not find ', entry
+        # Print name, size, created time, modified time
+        print entry.ljust(20),\
+            str(file_info.st_size).ljust(10),\
+            time.ctime(file_info.st_ctime).ljust(30),\
+            time.ctime(file_info.st_mtime).ljust(30)
 
 
 def get_size_directory(path):
     """
+    Get the size of the specified directory
     """
 
     # Get the files and directories in the path
     files = os.listdir(path)
 
+    # Variable for the total size
     total_size = 0
 
+    # Loop through each file
     for entry in files:
+        # Get the information of the file
         file_info = _get_entry_info(entry)
 
+        # If there is no file info
+        if file_info is None:
+            # Just jump to the next iteration of the loop
+            continue
+
+        # Add the size of the current file to the totla
         total_size += file_info.st_size
 
+    # Print the total size of the current directory
     print 'Size of current directory: ' + str(total_size)
+
+    # Return the total size
+    return total_size
 
 
 # Main method
@@ -91,7 +127,7 @@ def main(args):
             list_files(args[0])
 
     # If the user wants the size of the directory
-    elif arguments['-s'] or arguments['--size']:
+    elif arguments['s'] or arguments['size']:
         # Pass the current path
         get_size_directory('.')
 
