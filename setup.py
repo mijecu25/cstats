0d6c456bbab5e63a7b1c63e8e7afc58a5ccbddd9
Miguel Velez <miguelvelez@mijecu25.com>
2015-07-27 17:09:14 -0400

    Fixing python version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python "" 2.7'
+        'Programming Language :: Python :: 2.7'
     ],
     keywords=[
         'stats',
