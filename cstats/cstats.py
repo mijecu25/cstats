36f1f0bdaea04fb04f6c2951f7e6f3c4f6339167
Miguel Velez <miguelvelez@mijecu25.com>
2015-11-12 21:12:37 -0600

    Adding file types

--- cstats/cstats.py
+++ cstats/cstats.py
@@ -3,8 +3,9 @@
 cstats provides statistic and information about your directories
 
 Usage:
-    cstats
     cstats (ls | list) [<path>]
+    cstats (s | size) [<path>]
+    cstats (t | type) [<path>]
     cstats (-h | --help)
     cstats --version
 
@@ -13,20 +14,6 @@ Options:
     --version  Show version.
 
 """
-
-
-
-
-# Usage:
-#     cstats (ls | list) [<path>]
-#     cstats (s | size) [<path>]
-
-#     cstats -v | --version
-#
-# Options:
-#
-
-
 
 
 import sys as system
@@ -40,6 +27,7 @@ __version__ = '0.2.0.1'
 
 __cstats_version = 'cstats version "' + __version__ + '"\n' \
                                                       'author "' + __author__ + '"'
+__file_types = {'py': 'Python', 'java': 'Java'}
 
 
 def _get_entry_info(path, entry):
@@ -53,13 +41,9 @@ def _get_entry_info(path, entry):
         # Remove the '/'
         path = path[:-1]
 
-    # Try
-    # try:
-    #     # Get information and attributes from the entry in the path
+    # Get information and attributes from the entry in the path
     info = os.stat(path + '/' + entry)
-    # except WindowsError:
-    #     # The might be some windows error, so set the result to None
-    #     info = None
+
     # Return the information
     return info
 
@@ -85,13 +69,10 @@ def list_files(path):
             # Just jump to the next iteration of the loop
             continue
 
-        # print str(file_info)
-
         # Print name, size, created time, modified time
         print entry.ljust(20),\
             'size: ' + str(file_info.st_size).ljust(10),\
-            'modified: ' + time.ctime(file_info.st_mtime).ljust(30)
-            # time.ctime(file_info.st_birthtime).ljust(30),\
+            'modified: ' + time.ctime(file_info.st_mtime).ljust(50)
 
 
 def get_size_directory(path):
@@ -108,7 +89,7 @@ def get_size_directory(path):
     # Loop through each file
     for entry in files:
         # Get the information of the file
-        file_info = _get_entry_info(entry, path)
+        file_info = _get_entry_info(path, entry)
 
         # If there is no file info
         if file_info is None:
@@ -119,10 +100,44 @@ def get_size_directory(path):
         total_size += file_info.st_size
 
     # Print the total size of the current directory
-    print 'Size of current directory: ' + str(total_size)
+    print 'Size of current directory: ' + str(total_size) + ' bytes'
 
     # Return the total size
     return total_size
+
+
+def get_file_types(path):
+    """
+    Get a list of the file types of the specified directory
+    """
+
+    # Get the files and directories in the path
+    files = os.listdir(path)
+
+    file_types = {'Other': 0}
+
+    # Loop through each file
+    for entry in files:
+        # Split each file and extension
+        file_extension = entry.split('.')
+
+        print entry
+        # If the file has a name and extension or it begins with a dot
+        if len(file_extension) == 2 or entry[0] == '.':
+            # If the file extension is not in the file types
+            if file_extension[1] not in __file_types:
+                file_types['Other'] += 1
+            # If the file extension is already in the file types of the current directory
+            elif file_extension[1] in file_types:
+                # Increase the count of that file
+                file_types[file_extension[1]] += 1
+            else:
+                # Put a new entry of that file
+                file_types[file_extension[1]] = 1
+
+    print str(file_types)
+
+    return file_types
 
 
 # Main method
@@ -151,18 +166,27 @@ def main():
         else:
             # Else, pass the path provided by the user
             list_files(args[0])
+    # If the user wants the size of the directory
+    elif arguments['s'] or arguments['size']:
+        if len(args) == 0:
+            # Pass the current path
+            get_size_directory('.')
+        else:
+            # Else, pass the path provided by the user
+            get_size_directory(args[0])
+    # If the user wants alits of the types of files of the directory
+    elif arguments['t'] or arguments['type']:
+        if len(args) == 0:
+            # Pass the current path
+            get_file_types('.')
+        else:
+            # Else, pass the path provided by the user
+            get_file_types(args[0])
     else:
         # Print the man page
         print __doc__
 
-    # # If the user wants the size of the directory
-    # elif arguments['s'] or arguments['size']:
-    #     if len(args) == 0:
-    #         # Pass the current path
-    #         get_size_directory('.')
-    #     else:
-    #         # Else, pass the path provided by the user
-    #         get_size_directory(args[0])
+
     # else:
 
 
