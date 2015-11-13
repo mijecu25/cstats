004009191ddcb6542c5c79e9014e0f095f3aafc0
Miguel Velez <miguelvelez@mijecu25.com>
2015-11-13 11:13:33 -0600

    Added information about file types

--- cstats/cstats.py
+++ cstats/cstats.py
@@ -15,19 +15,34 @@ Options:
 
 """
 
-
 import sys as system
 import time
 import os
+import operator
 
 from docopt import docopt
 
 __author__ = 'Miguel Velez - miguelvelezmj25'
-__version__ = '0.2.0.1'
+__version__ = '0.2.0.3'
 
 __cstats_version = 'cstats version "' + __version__ + '"\n' \
                                                       'author "' + __author__ + '"'
-__file_types = {'py': 'Python', 'java': 'Java'}
+__music_extensions = {'3gp', 'act', 'aiff', 'aac', 'amr', 'ape', 'au', 'awb', 'dct', 'dss', 'dvf', 'flac', 'gsm',
+                      'iklax', 'ivs', 'm4a', 'm4p', 'mmf', 'mp3', 'mpc', 'msv', 'ogg', 'oga', 'opus', 'ra', 'rm',
+                      'raw', 'sln', 'tta', 'vox', 'wav', 'wma', 'wv', 'webm'}
+__photo_extensions = {'tif', 'tiff', 'gif', 'jpeg', 'jpg', 'jif', 'jfif', 'jp2', 'jpx', 'j2k', 'j2c', 'fpx', 'pcd',
+                      'png'}
+__document_extensions = {'pdf', 'doc', 'docx', 'log', 'msg', 'odt', 'pages', 'rtf', 'tex', 'txt', 'wpd', 'wps', 'csv',
+                         'dat', 'gbr', 'ged', 'key', 'keychain', 'pps', 'ppt', 'pptx', 'sdf', 'tar', 'vcf', 'xml',
+                         'xlr', 'xls', 'xlsx', 'db', 'dbf', 'mdb', 'pdb', 'sql', 'apk', 'jar', 'asp', 'aspx', 'cer',
+                         'cfm', 'csr', 'css', 'htm', 'html', 'js', 'jsp', 'php', 'rss', 'xhtml', '7z', 'cbr', 'deb',
+                         'gz', 'pkg', 'rar', 'rpm', 'sitx', 'tar.gz', 'zip', 'zipx', 'c', 'class', 'cpp', 'cs', 'dtd',
+                         'fla', 'h', 'java', 'lua', 'm', 'pl', 'py', 'sh', 'sln', 'swift', 'vcxproj', 'xcodeproj'}
+__video_extensions = {'webm', 'mkv', 'flv', 'vob', 'ogv', 'ogg', 'drc', 'gif', 'gifv', 'mng', 'avi', 'mov', 'qt',
+                      'wmv', 'yuv', 'rm', 'rmvb', 'asf', 'mp4', 'm4v', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', '.mpg',
+                      'mpeg,' 'm2v', 'svi', '3gp', '3g2', 'mxf', 'roq', 'nsv', 'flv', 'f4v', 'f4p', 'f4a', 'f4b'}
+__file_types = {'Music': __music_extensions, 'Photos': __photo_extensions, 'Documents': __document_extensions, 'Videos':
+                __video_extensions}
 
 
 def _get_entry_info(path, entry):
@@ -51,6 +66,7 @@ def _get_entry_info(path, entry):
 def list_files(path):
     """
     List the files and directories in the specified path.
+    :param path:
     """
 
     # Get the files and directories in the path
@@ -58,6 +74,8 @@ def list_files(path):
 
     # Sort the files by alphabetical order
     sorted(files)
+
+    print 'Total files ' + str(len(files))
 
     # Loop through each file
     for entry in files:
@@ -71,13 +89,14 @@ def list_files(path):
 
         # Print name, size, created time, modified time
         print entry.ljust(20),\
-            'size: ' + str(file_info.st_size).ljust(10),\
-            'modified: ' + time.ctime(file_info.st_mtime).ljust(50)
+            'size ' + str(file_info.st_size).ljust(10),\
+            'modified ' + time.ctime(file_info.st_mtime).ljust(50)
 
 
 def get_size_directory(path):
     """
     Get the size of the specified directory
+    :param path:
     """
 
     # Get the files and directories in the path
@@ -100,7 +119,7 @@ def get_size_directory(path):
         total_size += file_info.st_size
 
     # Print the total size of the current directory
-    print 'Size of current directory: ' + str(total_size) + ' bytes'
+    print 'Size of current directory ' + str(total_size) + ' bytes'
 
     # Return the total size
     return total_size
@@ -109,35 +128,62 @@ def get_size_directory(path):
 def get_file_types(path):
     """
     Get a list of the file types of the specified directory
+    :param path:
     """
 
     # Get the files and directories in the path
     files = os.listdir(path)
 
-    file_types = {'Other': 0}
+    # The dictionary with the counts of the file types
+    current_file_types = {'Other': 0, 'Folders': 0}
 
     # Loop through each file
     for entry in files:
         # Split each file and extension
         file_extension = entry.split('.')
 
-        print entry
+        # Variable to check if we added to an existing type
+        not_found = True
+
         # If the file has a name and extension or it begins with a dot
         if len(file_extension) == 2 or entry[0] == '.':
-            # If the file extension is not in the file types
-            if file_extension[1] not in __file_types:
-                file_types['Other'] += 1
-            # If the file extension is already in the file types of the current directory
-            elif file_extension[1] in file_types:
-                # Increase the count of that file
-                file_types[file_extension[1]] += 1
-            else:
-                # Put a new entry of that file
-                file_types[file_extension[1]] = 1
+            # For each file type in the map of existing types
+            for file_type in __file_types:
+                # If the file extensions is part of one of the current types
+                if file_extension[1] in __file_types[file_type]:
+                    # If there is already a count for the file type
+                    if file_type in current_file_types:
+                        # Increment the count by 1
+                        current_file_types[file_type] += 1
+                    else:
+                        # Create a new entry with the first element
+                        current_file_types[file_type] = 1
 
-    print str(file_types)
+                    # Since we increased the count, we found something
+                    not_found = False
+                    # Do not keep searching in other file types
+                    break
 
-    return file_types
+            # If we did not find anything associated to the file extension
+            if not_found:
+                # The file is of type other
+                current_file_types['Other'] += 1
+        else:
+            # The current entry is a folder
+            current_file_types['Folders'] += 1
+
+    # Sort the values in the map based on the counter in reverse order
+    sorted_types = sorted(current_file_types.items(), key=operator.itemgetter(1), reverse=True)
+
+    # For each file type in the file types
+    for file_type in sorted_types:
+        # If we found at least one element of this type
+        if file_type[1] > 0:
+            # Print the type and count
+            print file_type[0] + ' ' + str(file_type[1])
+
+    # Return the map of all of the types and counters
+    return current_file_types
 
 
 # Main method
@@ -185,9 +231,6 @@ def main():
     else:
         # Print the man page
         print __doc__
-
-
-    # else:
 
 
 # Start the program
